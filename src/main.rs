8d1545e25e1f1b9377e4917f96234fc3a1e3dfed
ash <ashkernel02@gmail.com>
2025-06-12 20:53:45 -0600

    taking examples from zip crate for some functions for zipping the archive up

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 use clap::Parser;
-use colored::{ColoredString, Colorize};
+use fs_extra::dir::get_size;
 use lazy_static::lazy_static;
+use serde_json::json;
 use tempfile::{TempDir, tempdir};
+use tokio::{
+    task::JoinHandle,
+    time::{Duration, sleep},
+};
 
-use tokio::time::{sleep, Duration};
+// taken from zip-rs examples
+use anyhow::Context;
+use zip::{result::ZipError, write::SimpleFileOptions};
+use walkdir::{DirEntry, WalkDir};
 
-use std::thread;
+use std::{
+    fs::File,
+    io::{Write, Seek, Read},
+    path::{Path, PathBuf},
+    thread,
+};
 
 mod crawl;
 use crawl::{DfsState, run_dfs};
 
 lazy_static! {
-    pub static ref TEMPDIR: TempDir = tempdir().unwrap();
-}
-
-fn log(s: ColoredString) {
-    println!("LOG: {}", s)
+    pub static ref TEMPDIR: TempDir =
+        tempdir().expect("couldn't instantiate a temporary directory");
 }
 
 type Url = String; // Rust probably has a better type for this
@@ -44,7 +54,14 @@ struct Args {
         value_name = "ARCHIVE_SIZE",
         help = "Maximum size of the produced archive, in MB."
     )]
-    tmpfs_size: Option<usize>,
+    tmpfs_size: Option<u64>,
+    #[clap(
+        short,
+        long,
+        value_name = "DESTINATION_ZIPFILE",
+        help = "Where to place the result archive."
+    )]
+    destination: PathBuf,
 }
 
 #[tokio::main]
@@ -56,6 +73,7 @@ async fn main() {
     let num_workers = thread::available_parallelism().unwrap().get();
     let num_workers = args.num_workers.unwrap_or(num_workers);
     let tmpfs_size = args.tmpfs_size.unwrap_or(200);
+    let dst_file = args.destination;
 
     let mut tasks = Vec::with_capacity(num_workers);
     let mut state = DfsState::new();
@@ -67,20 +85,93 @@ async fn main() {
         }));
     }
 
-    // TODO: setup background task checking for tmpfs size
-    tokio::task::spawn(async {
-        loop {
-            println!("waiting...");
-            sleep(Duration::from_millis(200)).await;
-        }
-    });
+    wait_loop(tmpfs_size, tasks).await;
 
-    for task in tasks {
-        task.await.unwrap();
+    let stats_path = TEMPDIR.path().join("stats.json");
+    let mut stats_file = File::create(stats_path).unwrap();
+    write!(stats_file, "{}", json!(*state.visited));
+
+    // TODO: zip TEMPDIR and put into an accessible archive
+
+}
+
+fn currentsize_tmpfs() -> u64 {
+    get_size(TEMPDIR.path()).unwrap()
+}
+
+async fn wait_loop(tmpfs_size: u64, tasks: Vec<JoinHandle<()>>) {
+    loop {
+        if currentsize_tmpfs() >= tmpfs_size {
+            for task in &tasks {
+                // TODO: should probably abort more gracefully
+                task.abort();
+                break;
+            }
+        }
+        sleep(Duration::from_millis(200)).await;
+    }
+}
+
+// compression taken from zip-rs examples
+// https://github.com/zip-rs/zip2/blob/0a40f7183959e311b3131d3e1f7392d166c26b2a/examples/write_dir.rs
+
+fn zip_dir<T>(
+    it: &mut dyn Iterator<Item = DirEntry>,
+    prefix: &Path,
+    writer: T,
+    method: zip::CompressionMethod,
+) -> anyhow::Result<()>
+where
+    T: Write + Seek,
+{
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = SimpleFileOptions::default()
+        .compression_method(method)
+        .unix_permissions(0o755);
+
+    let prefix = Path::new(prefix);
+    let mut buffer = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        let name = path.strip_prefix(prefix).unwrap();
+        let path_as_string = name
+            .to_str()
+            .map(str::to_owned)
+            .with_context(|| format!("{name:?} Is a Non UTF-8 Path"))?;
+
+        // Write file or directory explicitly
+        // Some unzip tools unzip files with directory paths correctly, some do not!
+        if path.is_file() {
+            println!("adding file {path:?} as {name:?} ...");
+            zip.start_file(path_as_string, options)?;
+            let mut f = File::open(path)?;
+
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&buffer)?;
+            buffer.clear();
+        } else if !name.as_os_str().is_empty() {
+            // Only if not root! Avoids path spec / warning
+            // and mapname conversion failed error on unzip
+            println!("adding dir {path_as_string:?} as {name:?} ...");
+            zip.add_directory(path_as_string, options)?;
+        }
+    }
+    zip.finish()?;
+    Ok(())
+}
+
+fn doit(src_dir: &Path, dst_file: &Path, method: zip::CompressionMethod) -> anyhow::Result<()> {
+    if !Path::new(src_dir).is_dir() {
+        return Err(ZipError::FileNotFound.into());
     }
 
-    // TODO: fetch all data and put into a zip file
-    // probably want to put a cap on how much data can be in the db
-    // then also want to emit some sort of file encoding what URLs are visited and how often
-    // probably not more complicated than simply writing a json string
+    let path = Path::new(dst_file);
+    let file = File::create(path).unwrap();
+
+    let walkdir = WalkDir::new(src_dir);
+    let it = walkdir.into_iter();
+
+    zip_dir(&mut it.filter_map(|e| e.ok()), src_dir, file, method)?;
+
+    Ok(())
 }
